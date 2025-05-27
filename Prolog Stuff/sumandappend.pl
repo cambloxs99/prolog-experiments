05b77f4442b715a4a1d70b643ab566763df23c0a
cambloxs99 <79669613+cambloxs99@users.noreply.github.com>
2025-05-27 10:21:52 +0100

    Update sumandappend.pl

--- Prolog Stuff/sumandappend.pl
+++ Prolog Stuff/sumandappend.pl
@@ -5,6 +5,7 @@ sum(X, Y, Z) :-
 
 % BASE CASE: List1 is an empty list and thus the appended list is all the values of List2.
 % The rule reduces to base case, when base case is true the recursion ends.
+% This is how the append/3 predicate already functions
 append3([], List2, List2).
 append3([FirstValue | List1], List2, [FirstValue | List1AndList2]) :-
     append3(List1, List2, List1AndList2).
